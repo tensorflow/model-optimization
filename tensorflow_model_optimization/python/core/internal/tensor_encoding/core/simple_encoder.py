e0665a29ea6a7796b888bd90da1e74f021b6e72a
Jakub Konecny <konkey@google.com>
2019-06-11 16:11:30 -0700

    Implements TF 2.0 focused SimpleEncoderV2, which will replace current SimpleEncode(s).
    There is no plan for ensuring any changes are backward-compatible, as suggested by the project's presence in the "internal/" directory.
    
    PiperOrigin-RevId: 252720045

--- tensorflow_model_optimization/python/core/internal/tensor_encoding/core/simple_encoder.py
+++ tensorflow_model_optimization/python/core/internal/tensor_encoding/core/simple_encoder.py
@@ -85,6 +85,186 @@ class SimpleEncoder(object):
       return encoded_structure, decode_fn
 
 
+# Temporary symbol; do not depend on.
+# TODO(konkey): Replace the other classes in this file by this class, which will
+# be renamed to SimpleEncoder.
+class SimpleEncoderV2(object):
+  """A simple class for encoding.
+
+  This class provides functionality for encoding in the "one-to-many" case,
+  where a `Tensor` is encoded in one location, and is to be decoded at
+  potentially many other locations, leaving the communication between encoding
+  and decoding up to the user.
+
+  An instance of this class is capable of encoding only values of a shape and
+  dtype, as specified at construction time. For example, an separate instance of
+  this class should be used for encoding every weight of a model, as opposed to
+  a single instance being reused for each weight.
+
+  `SimpleEncoderV2` exposes the state of the underlying encoder, and the user is
+  responsible for keeping track of the state in case the encoding should be
+  adaptive as part of an iterative process. If state is not needed, it can be
+  ignored when calling the `encode` method.
+  """
+
+  def __init__(self, encoder, tensorspec):
+    """Creates a `SimpleEncoderV2` for encoding `tensorspec`-like values.
+
+    This method instantiates `SimpleEncoderV2`, wrapping the functionality of
+    `encoder` and exposing necessary logic for encoding values compatible with
+    `tensorspec`. Note that the returned encoder will not accept inputs of other
+    properties.
+
+    Args:
+      encoder: An `Encoder` object to be used for encoding.
+      tensorspec: A `TensorSpec`. The created `SimpleEncoderV2` will be
+      constrained to only encode input values compatible with `tensorspec`.
+
+    Returns:
+      A `SimpleEncoderV2`.
+
+    Raises:
+      TypeError:
+        If `encoder` is not an `Encoder` or `tensorspec` is not a `TensorSpec`.
+    """
+    if not isinstance(encoder, core_encoder.Encoder):
+      raise TypeError('The encoder must be an instance of `Encoder`.')
+    if not isinstance(tensorspec, tf.TensorSpec):
+      raise TypeError('The tensorspec must be a tf.TensorSpec.')
+
+    # These dictionaries are filled inside of the initial_state_fn and encode_fn
+    # methods, to be used in encode_fn and decode_fn methods, respectively.
+    # Decorated by tf.function, their necessary side effects are realized during
+    # call to get_concrete_function(). Because of fixed input_signatures, these
+    # are traced only once. See the tf.function tutorial for more details on
+    # the tracing semantics.
+    state_py_structure = {}
+    encoded_py_structure = {}
+
+    @tf.function
+    def initial_state_fn():
+      state = encoder.initial_state()
+      assert not state_py_structure  # This should be traced only once.
+      state_py_structure['state'] = nest.map_structure(lambda _: None, state)
+      # Simplify the structure that needs to be manipulated by the user.
+      return tuple(nest.flatten(state))
+
+    @tf.function(input_signature=[
+        tensorspec,
+        nest.map_structure(
+            tf.TensorSpec.from_tensor,
+            initial_state_fn.get_concrete_function().structured_outputs)
+    ])  # pylint: disable=missing-docstring
+    def encode_fn(x, flat_state):
+      state = nest.pack_sequence_as(state_py_structure['state'], flat_state)
+      encode_params, decode_params = encoder.get_params(state)
+      encoded_x, state_update_tensors, input_shapes = encoder.encode(
+          x, encode_params)
+      updated_flat_state = tuple(
+          nest.flatten(encoder.update_state(state, state_update_tensors)))
+
+      # The following code converts the nested structres necessary for the
+      # underlying encoder, to a single flat dictionary, which is simpler to
+      # manipulate by the users of SimpleEncoderV2.
+      full_encoded_structure = {
+          _TENSORS: encoded_x,
+          _PARAMS: decode_params,
+          _SHAPES: input_shapes
+      }
+      flat_encoded_structure = dict(
+          nest.flatten_with_joined_string_paths(
+              full_encoded_structure, separator='/'))
+      flat_encoded_py_structure, flat_encoded_tf_structure = (
+          py_utils.split_dict_py_tf(flat_encoded_structure))
+
+      assert not encoded_py_structure  # This should be traced only once.
+      encoded_py_structure['full'] = nest.map_structure(lambda _: None,
+                                                        full_encoded_structure)
+      encoded_py_structure['flat_py'] = flat_encoded_py_structure
+      return flat_encoded_tf_structure, updated_flat_state
+
+    @tf.function(input_signature=[
+        nest.map_structure(
+            tf.TensorSpec.from_tensor,
+            encode_fn.get_concrete_function().structured_outputs[0])
+    ])  # pylint: disable=missing-docstring
+    def decode_fn(encoded_structure):
+      encoded_structure = py_utils.merge_dicts(encoded_structure,
+                                               encoded_py_structure['flat_py'])
+      encoded_structure = nest.pack_sequence_as(encoded_py_structure['full'],
+                                                nest.flatten(encoded_structure))
+      return encoder.decode(encoded_structure[_TENSORS],
+                            encoded_structure[_PARAMS],
+                            encoded_structure[_SHAPES])
+
+    # Ensures the decode_fn is traced during initialization.
+    decode_fn.get_concrete_function()
+
+    self._initial_state_fn = initial_state_fn
+    self._encode_fn = encode_fn
+    self._decode_fn = decode_fn
+
+  def initial_state(self, name=None):
+    """Returns the initial state.
+
+    Args:
+      name: `string`, name of the operation.
+
+    Returns:
+      A tuple of `Tensor` values, representing the initial state.
+    """
+    with tf.name_scope(name, 'simple_encoder_initial_state'):
+      return self._initial_state_fn()
+
+  def encode(self, x, state=None, name=None):
+    """Encodes the provided input.
+
+    If `state` is not provided, return value of the `initial_state` method will
+    be used.
+
+    Args:
+      x: A `Tensor` to be encoded.
+      state: The (optional) current state, matching the tuple returned by the
+        `initial_state` method.
+      name: `string`, name of the operation.
+
+    Returns:
+      A `(encoded_x, update_state)` tuple, where `encoded_x` is a dictionary of
+      `Tensor` values representing the encoded `x`, and `updated_state` is the
+      state updated after encoding, of the same structure as `state`.
+
+    Raises:
+      ValueError:
+        If `x` does not have the expected shape or dtype, or if `state` does not
+        have the same structure as return value of the `initial_state` method.
+    """
+    if state is None:
+      state = self.initial_state()
+    with tf.name_scope(name, 'simple_encoder_encode',
+                       [x] + list(state)):
+      return self._encode_fn(x, state)
+
+  def decode(self, encoded_x, name=None):
+    """Decodes the encoded value.
+
+    Args:
+      encoded_x: A dictionary of the same structure as returned by the `encode`
+        method. Represents the encoded value to be decoded.
+      name: `string`, name of the operation.
+
+    Returns:
+      A single `Tensor` of the same shape and dtype as the original input to the
+      `encode` method.
+
+    Raises:
+      ValueError:
+        If `encoded_x` is not of the same structure as returned by the `encode`
+        method.
+    """
+    with tf.name_scope(name, 'simple_encoder_decode', encoded_x.values()):
+      return self._decode_fn(encoded_x)
+
+
 class StatefulSimpleEncoder(object):
   """A simple stateful class for encoding.
 
--- tensorflow_model_optimization/python/core/internal/tensor_encoding/core/simple_encoder_test.py
+++ tensorflow_model_optimization/python/core/internal/tensor_encoding/core/simple_encoder_test.py
@@ -356,6 +356,200 @@ class StatefulSimpleEncoderTest(tf.test.TestCase, parameterized.TestCase):
       decode_fn({})
 
 
+class SimpleEncoderV2Test(tf.test.TestCase, parameterized.TestCase):
+
+  @tf.contrib.eager.run_test_in_graph_and_eager_modes
+  def test_basic_encode_decode(self):
+    """Tests basic encoding and decoding works as expected."""
+    x = tf.constant(1.0, tf.float32)
+    encoder = simple_encoder.SimpleEncoderV2(
+        core_encoder.EncoderComposer(
+            test_utils.PlusOneOverNEncodingStage()).make(),
+        tf.TensorSpec.from_tensor(x))
+
+    state = encoder.initial_state()
+    iteration = _make_iteration_function(encoder)
+    for i in range(1, 10):
+      x, encoded_x, decoded_x, state = self.evaluate(iteration(x, state))
+      self.assertAllClose(x, decoded_x)
+      self.assertAllClose(1.0 + 1 / i,
+                          _encoded_x_field(encoded_x, [TENSORS, PN_VALS]))
+
+  @tf.contrib.eager.run_test_in_graph_and_eager_modes
+  def test_composite_encoder(self):
+    """Tests functionality with a general, composite `Encoder`."""
+    x = tf.constant(1.2)
+    encoder = core_encoder.EncoderComposer(
+        test_utils.SignIntFloatEncodingStage())
+    encoder.add_child(test_utils.TimesTwoEncodingStage(), SIF_SIGNS)
+    encoder.add_child(test_utils.PlusOneEncodingStage(), SIF_INTS)
+    encoder.add_child(test_utils.TimesTwoEncodingStage(), SIF_FLOATS).add_child(
+        test_utils.PlusOneOverNEncodingStage(), T2_VALS)
+    encoder = simple_encoder.SimpleEncoderV2(encoder.make(),
+                                             tf.TensorSpec.from_tensor(x))
+
+    state = encoder.initial_state()
+    iteration = _make_iteration_function(encoder)
+    for i in range(1, 10):
+      x, encoded_x, decoded_x, state = self.evaluate(iteration(x, state))
+      self.assertAllClose(x, decoded_x)
+      self.assertAllClose(
+          2.0, _encoded_x_field(encoded_x, [TENSORS, SIF_SIGNS, T2_VALS]))
+      self.assertAllClose(
+          2.0, _encoded_x_field(encoded_x, [TENSORS, SIF_INTS, P1_VALS]))
+      self.assertAllClose(
+          0.4 + 1 / i,
+          _encoded_x_field(encoded_x, [TENSORS, SIF_FLOATS, T2_VALS, PN_VALS]))
+
+  @tf.contrib.eager.run_test_in_graph_and_eager_modes
+  def test_none_state_equal_to_initial_state(self):
+    """Tests that not providing state is the same as initial_state."""
+    x = tf.constant(1.0)
+    encoder = simple_encoder.SimpleEncoderV2(
+        core_encoder.EncoderComposer(
+            test_utils.PlusOneOverNEncodingStage()).make(),
+        tf.TensorSpec.from_tensor(x))
+
+    state = encoder.initial_state()
+    stateful_iteration = _make_iteration_function(encoder)
+
+    @tf.function
+    def stateless_iteration(x):
+      encoded_x, _ = encoder.encode(x)
+      decoded_x = encoder.decode(encoded_x)
+      return encoded_x, decoded_x
+
+    _, encoded_x_stateful, decoded_x_stateful, _ = self.evaluate(
+        stateful_iteration(x, state))
+    encoded_x_stateless, decoded_x_stateless = self.evaluate(
+        stateless_iteration(x))
+
+    self.assertAllClose(encoded_x_stateful, encoded_x_stateless)
+    self.assertAllClose(decoded_x_stateful, decoded_x_stateless)
+
+  @tf.contrib.eager.run_test_in_graph_and_eager_modes
+  def test_python_constants_not_exposed(self):
+    """Tests that only TensorFlow values are exposed to users."""
+    x = tf.constant(1.0)
+    tensorspec = tf.TensorSpec.from_tensor(x)
+    encoder_py = simple_encoder.SimpleEncoderV2(
+        core_encoder.EncoderComposer(
+            test_utils.SimpleLinearEncodingStage(2.0, 3.0)).make(), tensorspec)
+    a_var = tf.compat.v1.get_variable('a_var', initializer=2.0)
+    b_var = tf.compat.v1.get_variable('b_var', initializer=3.0)
+    encoder_tf = simple_encoder.SimpleEncoderV2(
+        core_encoder.EncoderComposer(
+            test_utils.SimpleLinearEncodingStage(a_var, b_var)).make(),
+        tensorspec)
+
+    state_py = encoder_py.initial_state()
+    state_tf = encoder_tf.initial_state()
+    iteration_py = _make_iteration_function(encoder_py)
+    iteration_tf = _make_iteration_function(encoder_tf)
+
+    self.evaluate(tf.compat.v1.global_variables_initializer())
+    _, encoded_x_py, decoded_x_py, _ = self.evaluate(iteration_py(x, state_py))
+    _, encoded_x_tf, decoded_x_tf, _ = self.evaluate(iteration_tf(x, state_tf))
+
+    # The encoded_x_tf should have two elements that encoded_x_py does not.
+    # These correspond to the two variables created passed on to constructor of
+    # encoder_tf, which are exposed as params. For encoder_py, these are python
+    # integers, and should thus be hidden from users.
+    self.assertLen(encoded_x_tf, len(encoded_x_py) + 2)
+
+    # Make sure functionality is still the same.
+    self.assertAllClose(x, decoded_x_tf)
+    self.assertAllClose(x, decoded_x_py)
+
+  @tf.contrib.eager.run_test_in_graph_and_eager_modes
+  def test_decode_needs_input_shape_static(self):
+    """Tests that mechanism for passing input shape works with static shape."""
+    x = tf.reshape(list(range(15)), [3, 5])
+    encoder = simple_encoder.SimpleEncoderV2(
+        core_encoder.EncoderComposer(
+            test_utils.ReduceMeanEncodingStage()).make(),
+        tf.TensorSpec.from_tensor(x))
+
+    state = encoder.initial_state()
+    iteration = _make_iteration_function(encoder)
+    _, _, decoded_x, _ = self.evaluate(iteration(x, state))
+    self.assertAllEqual([[7.0] * 5] * 3, decoded_x)
+
+  @tf.contrib.eager.run_test_in_graph_and_eager_modes
+  def test_decode_needs_input_shape_dynamic(self):
+    """Tests that mechanism for passing input shape works with dynamic shape."""
+    if tf.executing_eagerly():
+      fn = tf.function(test_utils.get_tensor_with_random_shape)
+      tensorspec = tf.TensorSpec.from_tensor(
+          fn.get_concrete_function().structured_outputs)
+      x = fn()
+    else:
+      x = test_utils.get_tensor_with_random_shape()
+      tensorspec = tf.TensorSpec.from_tensor(x)
+    encoder = simple_encoder.SimpleEncoderV2(
+        core_encoder.EncoderComposer(
+            test_utils.ReduceMeanEncodingStage()).make(), tensorspec)
+
+    # Validate the premise of the test - that encode mehtod expects an unknown
+    # shape. This should be true both for graph and eager mode.
+    assert (encoder._encode_fn.get_concrete_function().inputs[0].shape.as_list(
+    ) == [None])
+
+    state = encoder.initial_state()
+    iteration = _make_iteration_function(encoder)
+    x, _, decoded_x, _ = self.evaluate(iteration(x, state))
+    self.assertAllEqual(x.shape, decoded_x.shape)
+
+  @tf.contrib.eager.run_test_in_graph_and_eager_modes
+  def test_input_signature_enforced(self):
+    """Tests that encode/decode input signature is enforced."""
+    x = tf.constant(1.0)
+    encoder = simple_encoder.SimpleEncoderV2(
+        core_encoder.EncoderComposer(
+            test_utils.PlusOneOverNEncodingStage()).make(),
+        tf.TensorSpec.from_tensor(x))
+
+    state = encoder.initial_state()
+    with self.assertRaises(ValueError):
+      bad_x = tf.stack([x, x])
+      encoder.encode(bad_x, state)
+    with self.assertRaises(ValueError):
+      bad_state = state + (x,)
+      encoder.encode(x, bad_state)
+    encoded_x = encoder.encode(x, state)
+    with self.assertRaises(ValueError):
+      bad_encoded_x = dict(encoded_x)
+      bad_encoded_x.update({'x': x})
+      encoder.decode(bad_encoded_x)
+
+  @parameterized.parameters([1.0, 'str', object])
+  def test_not_an_encoder_raises(self, not_an_encoder):
+    """Tests invalid encoder argument."""
+    tensorspec = tf.TensorSpec((1,), tf.float32)
+    with self.assertRaisesRegex(TypeError, 'Encoder'):
+      simple_encoder.SimpleEncoderV2(not_an_encoder, tensorspec)
+
+  @parameterized.parameters([1.0, 'str', object])
+  def test_not_a_tensorspec_raises(self, bad_tensorspec):
+    """Tests invalid type of tensorspec argument."""
+    encoder = core_encoder.EncoderComposer(
+        test_utils.PlusOneOverNEncodingStage()).make()
+    with self.assertRaisesRegex(TypeError, 'TensorSpec'):
+      simple_encoder.SimpleEncoderV2(encoder, bad_tensorspec)
+
+
+def _make_iteration_function(encoder):
+  assert isinstance(encoder, simple_encoder.SimpleEncoderV2)
+
+  @tf.function
+  def iteration(x, state):
+    encoded_x, new_state = encoder.encode(x, state)
+    decoded_x = encoder.decode(encoded_x)
+    return x, encoded_x, decoded_x, new_state
+
+  return iteration
+
+
 def _encoded_x_field(encoded_x, path):
   """Returns a field from `encoded_x` returned by the `encode` method.
 
