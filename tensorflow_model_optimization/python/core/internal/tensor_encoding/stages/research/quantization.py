4accaa51f659ff2a34150d1ad7bd7fd3c9f9ba12
Yinlin Deng <yldeng@google.com>
2019-08-01 18:20:45 -0700

    Implements per-channel quantization encoding stage.
    PiperOrigin-RevId: 261239396

--- tensorflow_model_optimization/python/core/internal/tensor_encoding/stages/research/quantization.py
+++ tensorflow_model_optimization/python/core/internal/tensor_encoding/stages/research/quantization.py
@@ -108,8 +108,9 @@ class PRNGUniformQuantizationEncodingStage(encoding_stage.EncodingStageInterface
     # Randomized rounding.
     floored_x = tf.floor(x)
     random_seed = tf.random.uniform((2,), maxval=tf.int64.max, dtype=tf.int64)
-    num_elements = x.shape.num_elements()
-    rounding_floats = tf_utils.random_floats(num_elements, random_seed, x.dtype)
+    num_elements = tf.reduce_prod(tf.shape(x))
+    rounding_floats = tf.reshape(
+        self._random_floats(num_elements, random_seed, x.dtype), tf.shape(x))
 
     bernoulli = rounding_floats < (x - floored_x)
     quantized_x = floored_x + tf.cast(bernoulli, x.dtype)
@@ -138,10 +139,11 @@ class PRNGUniformQuantizationEncodingStage(encoding_stage.EncodingStageInterface
     max_value = tf.cast(decode_params[self.MAX_INT_VALUE_PARAMS_KEY],
                         quantized_x.dtype)
 
-    num_elements = quantized_x.shape.num_elements()
+    num_elements = tf.reduce_prod(tf.shape(quantized_x))
     # The rounding_floats are identical to those used in the encode method.
-    rounding_floats = tf_utils.random_floats(num_elements, random_seed,
-                                             min_x.dtype)
+    rounding_floats = tf.reshape(
+        self._random_floats(num_elements, random_seed, min_x.dtype),
+        tf.shape(quantized_x))
 
     # Regenerating the random values used in encode, enables us to determine a
     # narrower range of possible original values, before quantization was
@@ -156,3 +158,280 @@ class PRNGUniformQuantizationEncodingStage(encoding_stage.EncodingStageInterface
 
     x = q_shifted / max_value * (max_x - min_x) + min_x
     return x
+
+  def _random_floats(self, num_elements, seed, dtype):
+    return tf_utils.random_floats(num_elements, seed, dtype)
+
+
+@encoding_stage.tf_style_encoding_stage
+class PerChannelUniformQuantizationEncodingStage(
+    encoding_stage.EncodingStageInterface):
+  """Encoding stage performing uniform quantization per channel in conv layers.
+
+  This encoding stage will first reshape the input `x` to `(-1, dim)`, where
+  `dim` is the size of the last dimension of `x`. Then each of the `dim`
+  vectors will be quantized independently.
+
+  This encoding stage does not require the shape of the input `x` to the
+  `encode` method to be statically known.
+
+  In particular, given a floating point input `x` to the `encode` method, the
+  output will have the same `dtype` as `x`, with values being "floating point
+  integers" in the range `[0, 2**bits-1]`.
+
+  The extreme points of the quantized interval will correspond to the min and
+  max values of the input `x`.
+  """
+
+  ENCODED_VALUES_KEY = 'quantized_values'
+  MIN_MAX_VALUES_KEY = 'min_max'
+  MAX_INT_VALUE_PARAMS_KEY = 'max_value'
+  SEED_PARAMS_KEY = 'seed'
+  # The allowed values for `bits` argument to initializer.
+  # We cap the allowed quantization bits at 16, as the randomized rounding could
+  # otherwise be numerically unstable for float32 values.
+  _ALLOWED_BITS_ARG = list(range(1, 17))
+
+  def __init__(self, bits=8, stochastic=True):
+    """Initializer for the PerChannelUniformQuantizationEncodingStage.
+
+    Args:
+      bits: The number of bits to quantize to. Must be an integer between 1 and
+        16. Can be either a TensorFlow or a Python value.
+      stochastic: A Python bool, whether to use stochastic or deterministic
+        rounding. If `True`, the encoding is randomized and on expectation
+        unbiased. If `False`, the encoding is deterministic.
+
+    Raises:
+      ValueError: The inputs do not satisfy the above constraints.
+    """
+    if (not tf.contrib.framework.is_tensor(bits) and
+        bits not in self._ALLOWED_BITS_ARG):
+      raise ValueError('The bits argument must be an integer between 1 and 16.')
+    self._bits = bits
+
+    if not isinstance(stochastic, bool):
+      raise TypeError('The stochastic argument must be a bool.')
+    self._stochastic = stochastic
+
+  @property
+  def name(self):
+    """See base class."""
+    return 'per_channel_uniform_quantization'
+
+  @property
+  def compressible_tensors_keys(self):
+    """See base class."""
+    return [self.ENCODED_VALUES_KEY]
+
+  @property
+  def commutes_with_sum(self):
+    """See base class."""
+    return False
+
+  @property
+  def decode_needs_input_shape(self):
+    """See base class."""
+    return True
+
+  def get_params(self):
+    """See base class."""
+    params = {self.MAX_INT_VALUE_PARAMS_KEY: 2**self._bits - 1}
+    return params, params
+
+  def encode(self, x, encode_params):
+    """See base class."""
+    dim = tf.shape(x)[-1]
+    x = tf.reshape(x, [-1, dim])
+
+    # Per-channel min and max.
+    min_x = tf.reduce_min(x, axis=0)
+    max_x = tf.reduce_max(x, axis=0)
+
+    max_value = tf.cast(encode_params[self.MAX_INT_VALUE_PARAMS_KEY], x.dtype)
+    # Shift the values to range [0, max_value].
+    # In the case of min_x == max_x, this will return all zeros.
+    x = tf.div_no_nan(x - min_x, max_x - min_x) * max_value
+    if self._stochastic:  # Randomized rounding.
+      floored_x = tf.floor(x)
+      bernoulli = tf.random_uniform(tf.shape(x), dtype=x.dtype)
+      bernoulli = bernoulli < (x - floored_x)
+      quantized_x = floored_x + tf.cast(bernoulli, x.dtype)
+    else:  # Deterministic rounding.
+      quantized_x = tf.round(x)
+
+    encoded_tensors = {
+        self.ENCODED_VALUES_KEY: quantized_x,
+        self.MIN_MAX_VALUES_KEY: tf.stack([min_x, max_x])
+    }
+
+    return encoded_tensors
+
+  def decode(self,
+             encoded_tensors,
+             decode_params,
+             num_summands=None,
+             shape=None):
+    """See base class."""
+    quantized_x = encoded_tensors[self.ENCODED_VALUES_KEY]
+    min_max = encoded_tensors[self.MIN_MAX_VALUES_KEY]
+    min_x, max_x = min_max[0], min_max[1]
+    max_value = tf.cast(decode_params[self.MAX_INT_VALUE_PARAMS_KEY],
+                        quantized_x.dtype)
+
+    x = quantized_x / max_value * (max_x - min_x) + min_x
+
+    x = tf.reshape(x, shape)
+
+    return x
+
+
+@encoding_stage.tf_style_encoding_stage
+class PerChannelPRNGUniformQuantizationEncodingStage(
+    encoding_stage.EncodingStageInterface):
+  """Encoding stage performing uniform quantization per channel in conv layers.
+
+  This encoding stage will first reshape the input `x` to `(-1, dim)`, where
+  `dim` is the size of the last dimension of `x`. Then each of the `dim`
+  vectors will be quantized independently, using PRNG uniform quantization.
+
+  PRNG uniform quantization uses a single integer seed as the same source of
+  randomness in encode and decode methods, in order to, in the decode method,
+  differently estimate the original values before encoding was applied.
+
+  This encoding stage does not require the shape of the input `x` to the
+  `encode` method to be statically known.
+
+  In particular, given a floating point input `x` to the `encode` method, the
+  output will have the same `dtype` as `x`, with values being "floating point
+  integers" in the range `[0, 2**bits-1]`.
+
+  The extreme points of the quantized interval will correspond to the min and
+  max values of the input `x`.
+
+  The output of the `decode` method is an unbiased estimation of the original
+  values before quantization, but note that it is possible for some of the
+  decoded values to be outside of the original range.
+  """
+
+  ENCODED_VALUES_KEY = 'quantized_values'
+  MIN_MAX_VALUES_KEY = 'min_max'
+  MAX_INT_VALUE_PARAMS_KEY = 'max_value'
+  SEED_PARAMS_KEY = 'seed'
+  # The allowed values for `bits` argument to initializer.
+  # We cap the allowed quantization bits at 16, as the randomized rounding could
+  # otherwise be numerically unstable for float32 values.
+  _ALLOWED_BITS_ARG = list(range(1, 17))
+
+  def __init__(self, bits=8):
+    """Initializer for the `PerChannelPRNGUniformQuantizationEncodingStage`.
+
+    Args:
+      bits: The number of bits to quantize to. Must be an integer between 1 and
+        16. Can be either a TensorFlow or a Python value.
+
+    Raises:
+      ValueError: The inputs do not satisfy the above constraints.
+    """
+    if (not tf.contrib.framework.is_tensor(bits) and
+        bits not in self._ALLOWED_BITS_ARG):
+      raise ValueError('The bits argument must be an integer between 1 and 16.')
+    self._bits = bits
+
+  @property
+  def name(self):
+    """See base class."""
+    return 'per_channel_prng_uniform_quantization'
+
+  @property
+  def compressible_tensors_keys(self):
+    """See base class."""
+    return [self.ENCODED_VALUES_KEY]
+
+  @property
+  def commutes_with_sum(self):
+    """See base class."""
+    return False
+
+  @property
+  def decode_needs_input_shape(self):
+    """See base class."""
+    return True
+
+  def get_params(self):
+    """See base class."""
+    params = {self.MAX_INT_VALUE_PARAMS_KEY: 2**self._bits - 1}
+    return params, params
+
+  def encode(self, x, encode_params):
+    """See base class."""
+    dim = tf.shape(x)[-1]
+    x = tf.reshape(x, [-1, dim])
+
+    # Per-channel min and max.
+    min_x = tf.reduce_min(x, axis=0)
+    max_x = tf.reduce_max(x, axis=0)
+
+    max_value = tf.cast(encode_params[self.MAX_INT_VALUE_PARAMS_KEY], x.dtype)
+    # Shift the values to range [0, max_value].
+    # In the case of min_x == max_x, this will return all zeros.
+    x = tf.div_no_nan(x - min_x, max_x - min_x) * max_value
+
+    # Randomized rounding.
+    floored_x = tf.floor(x)
+    random_seed = tf.random.uniform((2,), maxval=tf.int64.max, dtype=tf.int64)
+    num_elements = tf.reduce_prod(tf.shape(x))
+    rounding_floats = tf.reshape(
+        self._random_floats(num_elements, random_seed, x.dtype), tf.shape(x))
+
+    bernoulli = rounding_floats < (x - floored_x)
+    quantized_x = floored_x + tf.cast(bernoulli, x.dtype)
+
+    # Include the random seed in the encoded tensors so that it can be used to
+    # generate the same random sequence in the decode method.
+    encoded_tensors = {
+        self.ENCODED_VALUES_KEY: quantized_x,
+        self.SEED_PARAMS_KEY: random_seed,
+        self.MIN_MAX_VALUES_KEY: tf.stack([min_x, max_x])
+    }
+
+    return encoded_tensors
+
+  def decode(self,
+             encoded_tensors,
+             decode_params,
+             num_summands=None,
+             shape=None):
+    """See base class."""
+    quantized_x = encoded_tensors[self.ENCODED_VALUES_KEY]
+    random_seed = encoded_tensors[self.SEED_PARAMS_KEY]
+    min_max = encoded_tensors[self.MIN_MAX_VALUES_KEY]
+    min_x, max_x = min_max[0], min_max[1]
+    max_value = tf.cast(decode_params[self.MAX_INT_VALUE_PARAMS_KEY],
+                        quantized_x.dtype)
+
+    num_elements = tf.reduce_prod(tf.shape(quantized_x))
+    # The rounding_floats are identical to those used in the encode method.
+    rounding_floats = tf.reshape(
+        self._random_floats(num_elements, random_seed, min_x.dtype),
+        tf.shape(quantized_x))
+
+    # Regenerating the random values used in encode, enables us to determine a
+    # narrower range of possible original values, before quantization was
+    # applied. We shift the quantized values into the middle of this range,
+    # corresponding to the intersection of
+    # [quantized_x - 1 + rounding_floats, quantized_x + rounding_floats]
+    # in the quantized range. This shifted value can be out of the range
+    # [0, max_value] and therefore the decoded value can be out of the range
+    # [min_x, max_x], which is impossible, but it ensures that the decoded x
+    # is an unbiased estimator of the original values before quantization.
+    q_shifted = quantized_x + rounding_floats - 0.5
+
+    x = q_shifted / max_value * (max_x - min_x) + min_x
+
+    x = tf.reshape(x, shape)
+
+    return x
+
+  def _random_floats(self, num_elements, seed, dtype):
+    return tf_utils.random_floats(num_elements, seed, dtype)
--- tensorflow_model_optimization/python/core/internal/tensor_encoding/stages/research/quantization_test.py
+++ tensorflow_model_optimization/python/core/internal/tensor_encoding/stages/research/quantization_test.py
@@ -111,6 +111,259 @@ class PRNGUniformQuantizationEncodingStageTest(test_utils.BaseEncodingStageTest
     with self.assertRaisesRegexp(ValueError, 'integer between 1 and 16'):
       quantization.PRNGUniformQuantizationEncodingStage(bits=bits)
 
+  def test_dynamic_input_shape(self):
+    # Tests that encoding works when the input shape is not statically known.
+    stage = quantization.PRNGUniformQuantizationEncodingStage(bits=8)
+    shape = [10, 5, 7]
+    prob = [0.5, 0.8, 0.6]
+    original_x = tf.random.uniform(shape, dtype=tf.float32)
+    rand = [tf.random.uniform([shape[i],]) for i in range(3)]
+    sample_indices = [
+        tf.reshape(tf.where(rand[i] < prob[i]), [-1]) for i in range(3)
+    ]
+    x = tf.gather(original_x, sample_indices[0], axis=0)
+    x = tf.gather(x, sample_indices[1], axis=1)
+    x = tf.gather(x, sample_indices[2], axis=2)
+
+    encode_params, decode_params = stage.get_params()
+    encoded_x, decoded_x = self.encode_decode_x(stage, x, encode_params,
+                                                decode_params)
+    test_data = test_utils.TestData(x, encoded_x, decoded_x)
+    test_data = self.evaluate_test_data(test_data)
+
+
+class PerChannelUniformQuantizationEncodingStageTest(
+    test_utils.BaseEncodingStageTest):
+  """Test for PerChannelUniformQuantizationEncodingStage."""
+
+  def default_encoding_stage(self):
+    """See base class."""
+    return quantization.PerChannelUniformQuantizationEncodingStage()
+
+  def default_input(self):
+    """See base class."""
+    return tf.random.uniform([50], minval=-1.0, maxval=1.0)
+
+  @property
+  def is_lossless(self):
+    """See base class."""
+    return False
+
+  def common_asserts_for_test_data(self, data):
+    """See base class."""
+    self._assert_is_integer_float(
+        data.encoded_x[quantization.PerChannelUniformQuantizationEncodingStage
+                       .ENCODED_VALUES_KEY])
+
+  def _assert_is_integer_float(self, quantized_vals):
+    """Asserts that float type values are integers."""
+    assert quantized_vals.dtype == np.float32
+    self.assertAllClose(quantized_vals,
+                        tf.cast(tf.cast(quantized_vals, np.int32), np.float32))
+
+  @parameterized.parameters(itertools.product([1, 2, 3, 4, 7, 8, 9, 16]))
+  def test_quantization_bits_stochastic_rounding(self, bits):
+    stage = quantization.PerChannelUniformQuantizationEncodingStage(
+        bits=bits, stochastic=True)
+    test_data = self.run_one_to_many_encode_decode(stage, self.default_input)
+    self._assert_is_integer_float(test_data.encoded_x[
+        quantization.PerChannelUniformQuantizationEncodingStage
+        .ENCODED_VALUES_KEY])
+    # For stochastic rounding, the potential error incurred by quantization
+    # is bounded by the range of the input values divided by the number of
+    # quantization buckets.
+    self.assertAllClose(
+        test_data.x, test_data.decoded_x, rtol=0.0, atol=2 / (2**bits - 1))
+
+  @parameterized.parameters(itertools.product([1, 2, 3, 4, 7, 8, 9, 16]))
+  def test_quantization_bits_deterministic_rounding(self, bits):
+    stage = quantization.PerChannelUniformQuantizationEncodingStage(
+        bits=bits, stochastic=False)
+    test_data = self.run_one_to_many_encode_decode(stage, self.default_input)
+    self._assert_is_integer_float(test_data.encoded_x[
+        quantization.PerChannelUniformQuantizationEncodingStage
+        .ENCODED_VALUES_KEY])
+    # For deterministic rounding, the potential error incurred by quantization
+    # is bounded by half of the range of the input values divided by the number
+    # of quantization buckets.
+    self.assertAllClose(
+        test_data.x, test_data.decoded_x, rtol=0.0, atol=1 / (2**bits - 1))
+
+  def test_quantization_empirically_unbiased(self):
+    # Tests that the quantization with stochastic=True "seems" to be unbiased.
+    # Executing the encoding and decoding many times, the average error should
+    # be a lot larger than the error of average decoded value.
+    x = tf.constant(np.random.rand((50)).astype(np.float32))
+    stage = quantization.PerChannelUniformQuantizationEncodingStage(
+        bits=2, stochastic=True)
+    encode_params, decode_params = stage.get_params()
+    encoded_x, decoded_x = self.encode_decode_x(stage, x, encode_params,
+                                                decode_params)
+    test_data = test_utils.TestData(x, encoded_x, decoded_x)
+    test_data_list = [self.evaluate_test_data(test_data) for _ in range(200)]
+
+    norm_errors = []
+    errors = []
+    for data in test_data_list:
+      norm_errors.append(np.linalg.norm(data.x - data.decoded_x))
+      errors.append(data.x - data.decoded_x)
+    mean_of_errors = np.mean(norm_errors)
+    error_of_mean = np.linalg.norm(np.mean(errors, axis=0))
+    self.assertGreaterEqual(mean_of_errors, error_of_mean * 10)
+
+  def test_all_zero_input_works(self):
+    # Tests that encoding does not blow up with all-zero input. With
+    # min_max=None, the derived min and max are identical, thus potential for
+    # division by zero.
+    stage = quantization.PerChannelUniformQuantizationEncodingStage(bits=8)
+    test_data = self.run_one_to_many_encode_decode(stage,
+                                                   lambda: tf.zeros([50]))
+    self.assertAllEqual(np.zeros((50)).astype(np.float32), test_data.decoded_x)
+
+  @parameterized.parameters([0, 17, -1, 1.5])
+  def test_bits_out_of_range_raises(self, bits):
+    with self.assertRaisesRegexp(ValueError, 'integer between 1 and 16'):
+      quantization.PerChannelUniformQuantizationEncodingStage(bits=bits)
+
+  def test_stochastic_tensor_raises(self):
+    with self.assertRaisesRegexp(TypeError, 'stochastic'):
+      quantization.PerChannelUniformQuantizationEncodingStage(
+          stochastic=tf.constant(True, dtype=tf.bool))
+
+  def test_dynamic_input_shape(self):
+    # Tests that encoding works when the input shape is not statically known.
+    stage = quantization.PerChannelUniformQuantizationEncodingStage(bits=8)
+    shape = [10, 5, 7]
+    prob = [0.5, 0.8, 0.6]
+    original_x = tf.random.poisson(1.5, shape, dtype=tf.float32)
+    rand = [tf.random.uniform([
+        shape[i],
+    ], dtype=tf.float32) for i in range(3)]
+    sample_indices = [
+        tf.reshape(tf.where(rand[i] < prob[i]), [-1]) for i in range(3)
+    ]
+    x = tf.gather(original_x, sample_indices[0], axis=0)
+    x = tf.gather(x, sample_indices[1], axis=1)
+    x = tf.gather(x, sample_indices[2], axis=2)
+
+    encode_params, decode_params = stage.get_params()
+    encoded_x, decoded_x = self.encode_decode_x(stage, x, encode_params,
+                                                decode_params)
+    test_data = test_utils.TestData(x, encoded_x, decoded_x)
+    test_data = self.evaluate_test_data(test_data)
+
+
+class PerChannelPRNGUniformQuantizationEncodingStageTest(
+    test_utils.BaseEncodingStageTest):
+  """Test for PerChannelPRNGUniformQuantizationEncodingStage."""
+
+  def default_encoding_stage(self):
+    """See base class."""
+    return quantization.PerChannelPRNGUniformQuantizationEncodingStage()
+
+  def default_input(self):
+    """See base class."""
+    return tf.random.uniform([50], minval=-1.0, maxval=1.0)
+
+  @property
+  def is_lossless(self):
+    """See base class."""
+    return False
+
+  def common_asserts_for_test_data(self, data):
+    """See base class."""
+    self._assert_is_integer_float(data.encoded_x[
+        quantization.PerChannelPRNGUniformQuantizationEncodingStage
+        .ENCODED_VALUES_KEY])
+
+  def _assert_is_integer_float(self, quantized_vals):
+    """Asserts that float type values are integers."""
+    assert quantized_vals.dtype == np.float32
+    self.assertAllClose(quantized_vals,
+                        tf.cast(tf.cast(quantized_vals, np.int32), np.float32))
+
+  @parameterized.parameters(itertools.product([1, 2, 3, 4, 7, 8, 9, 16]))
+  def test_quantization_bits_stochastic_rounding(self, bits):
+    stage = quantization.PerChannelPRNGUniformQuantizationEncodingStage(
+        bits=bits)
+    test_data = self.run_one_to_many_encode_decode(stage, self.default_input)
+    self._assert_is_integer_float(test_data.encoded_x[
+        quantization.PerChannelPRNGUniformQuantizationEncodingStage
+        .ENCODED_VALUES_KEY])
+    # For stochastic rounding, the potential error incurred by quantization
+    # is bounded by the range of the input values divided by the number of
+    # quantization buckets.
+    self.assertAllClose(
+        test_data.x, test_data.decoded_x, rtol=0.0, atol=2 / (2**bits - 1))
+
+  def test_quantization_empirically_unbiased(self):
+    # Tests that the quantization "seems" to be unbiased.
+    # Executing the encoding and decoding many times, the average error should
+    # be a lot larger than the error of average decoded value.
+    x = tf.constant(np.random.rand((50)).astype(np.float32))
+    stage = quantization.PerChannelPRNGUniformQuantizationEncodingStage(bits=2)
+    encode_params, decode_params = stage.get_params()
+    encoded_x, decoded_x = self.encode_decode_x(stage, x, encode_params,
+                                                decode_params)
+    test_data = test_utils.TestData(x, encoded_x, decoded_x)
+    test_data_list = [self.evaluate_test_data(test_data) for _ in range(200)]
+
+    norm_errors = []
+    errors = []
+    for data in test_data_list:
+      norm_errors.append(np.linalg.norm(data.x - data.decoded_x))
+      errors.append(data.x - data.decoded_x)
+    mean_of_errors = np.mean(norm_errors)
+    error_of_mean = np.linalg.norm(np.mean(errors, axis=0))
+    self.assertGreaterEqual(mean_of_errors, error_of_mean * 10)
+
+  @parameterized.parameters(itertools.product([tf.float32, tf.float64]))
+  def test_input_types(self, x_dtype):
+    # Tests combinations of input dtypes.
+    stage = quantization.PerChannelPRNGUniformQuantizationEncodingStage(bits=8)
+    x = tf.random.normal([50], dtype=x_dtype)
+    encode_params, decode_params = stage.get_params()
+    encoded_x, decoded_x = self.encode_decode_x(stage, x, encode_params,
+                                                decode_params)
+    test_data = test_utils.TestData(x, encoded_x, decoded_x)
+    test_data = self.evaluate_test_data(test_data)
+
+  def test_all_zero_input_works(self):
+    # Tests that encoding does not blow up with all-zero input. With
+    # min_max=None, the derived min and max are identical, thus potential for
+    # division by zero.
+    stage = quantization.PerChannelPRNGUniformQuantizationEncodingStage(bits=8)
+    test_data = self.run_one_to_many_encode_decode(stage,
+                                                   lambda: tf.zeros([50]))
+    self.assertAllEqual(np.zeros((50)).astype(np.float32), test_data.decoded_x)
+
+  @parameterized.parameters([0, 17, -1, 1.5])
+  def test_bits_out_of_range_raises(self, bits):
+    with self.assertRaisesRegexp(ValueError, 'integer between 1 and 16'):
+      quantization.PerChannelPRNGUniformQuantizationEncodingStage(bits=bits)
+
+  def test_dynamic_input_shape(self):
+    # Tests that encoding works when the input shape is not statically known.
+    stage = quantization.PerChannelPRNGUniformQuantizationEncodingStage(bits=8)
+    shape = [10, 5, 7]
+    prob = [0.5, 0.8, 0.6]
+    original_x = tf.random.poisson(1.5, shape, dtype=tf.float32)
+    rand = [tf.random.uniform([
+        shape[i],
+    ], dtype=tf.float32) for i in range(3)]
+    sample_indices = [
+        tf.reshape(tf.where(rand[i] < prob[i]), [-1]) for i in range(3)
+    ]
+    x = tf.gather(original_x, sample_indices[0], axis=0)
+    x = tf.gather(x, sample_indices[1], axis=1)
+    x = tf.gather(x, sample_indices[2], axis=2)
+
+    encode_params, decode_params = stage.get_params()
+    encoded_x, decoded_x = self.encode_decode_x(stage, x, encode_params,
+                                                decode_params)
+    test_data = test_utils.TestData(x, encoded_x, decoded_x)
+    test_data = self.evaluate_test_data(test_data)
+
 
 if __name__ == '__main__':
   tf.test.main()
