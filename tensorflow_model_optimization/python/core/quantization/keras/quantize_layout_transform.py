94a37b9ec1133f63703110217694066d6e873ac5
Pulkit Bhuwalka <pulkitb@google.com>
2019-10-02 12:32:16 -0700

    QuantizeLayoutTransform interface
    Creates the interface to be used for modifying
    model structure.
    
    PiperOrigin-RevId: 272495068

---
+++ tensorflow_model_optimization/python/core/quantization/keras/quantize_layout_transform.py
@@ -0,0 +1,54 @@
+# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Abstract Base Class for quantization transformations to a keras model.
+
+Keras models need certain transformations for quantization to exactly match the
+behavior of the backend they will be implemented on. This is important for
+improving model performance.
+
+This interface abstracts that behavior. Different backends can implement their
+own version.
+
+Module: tfmot.quantization.keras
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import abc
+import six
+
+
+@six.add_metaclass(abc.ABCMeta)
+class QuantizeLayoutTransform(object):
+  """Apply transformations to the model.
+
+  Transforms the original model to perform better while quantized
+  and also match the layout of the target backend.
+  """
+
+  @abc.abstractmethod
+  def apply(self, model):
+    """Transform model to a quantization friendly model.
+
+    Args:
+      model: Keras model to be quantized.
+
+    Returns:
+      New keras model based on `model` which has been
+      transformed to match the layout of the target backend.
+    """
+    raise NotImplementedError('Must be implemented in subclasses.')
